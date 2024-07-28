7d81e8ea76755642de4fb88fe76badc8978a0e5c
Fred <makflay85zet@gmail.com>
2024-07-28 15:44:51 +0400

    DZ7 added some changed

--- DZ_7/src/main/java/com/example/DZSeven/Security/PasswordGenerator.java
+++ DZ_7/src/main/java/com/example/DZSeven/Security/PasswordGenerator.java
@@ -18,21 +18,4 @@ public class PasswordGenerator {
     }
 }
 
-//package com.example.DZfive.Security;
-//
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//public class PasswordGenerator {
-//    public static void main(String[] args) {
-//        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//        String hashedPassword = passwordEncoder.encode("admin");
-//        System.out.println("Hashed admin password: " + hashedPassword);
-//
-//        hashedPassword = passwordEncoder.encode("user");
-//        System.out.println("Hashed user password: " + hashedPassword);
-//
-//        hashedPassword = passwordEncoder.encode("rest");
-//        System.out.println("Hashed rest password: " + hashedPassword);
-//    }
-//}
+
