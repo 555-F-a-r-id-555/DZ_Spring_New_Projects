eb12bd07f2d6017be175f99e6422e2963243a192
Fred <makflay85zet@gmail.com>
2024-08-12 01:04:11 +0400

    DZ10 some changed5

--- DZ_10/src/test/java/com/example/DZTen/Calculator/TaxCalculatorTest.java
+++ DZ_10/src/test/java/com/example/DZTen/Calculator/TaxCalculatorTest.java
@@ -5,19 +5,26 @@ import com.example.DZTen.Calculator.example.TaxResolver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
 class TaxCalculatorTest {
+
+    @Mock
+    TaxResolver mock;
 
     @Test
     void testGetPriceWithTax(){
-        TaxResolver mock = mock(TaxResolver.class);
+//        TaxResolver mock = mock(TaxResolver.class);
         when(mock.getCurrentTax()).thenReturn(0.5);
         TaxCalculator taxCalculator = new TaxCalculator(mock);
 
-       Assertions.assertEquals(150.0,taxCalculator.getPriceWithTax(100.0),0.000009);
+        Assertions.assertEquals(150.0,taxCalculator.getPriceWithTax(100.0),0.000009);
     }
-
 
 }
