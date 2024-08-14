67c0423489e3ec82a0fffc754dfa70d3f4633624
Fred <makflay85zet@gmail.com>
2024-08-15 03:30:33 +0400

    DZ added

---
+++ DZ_4/src/test/java/com/example/test04/Test03ApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.test04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * The type Test 03 application tests.
+ */
+@SpringBootTest
+class Test03ApplicationTests {
+
+    /**
+     * Context loads.
+     */
+    @Test
+	void contextLoads() {
+	}
+
+}
